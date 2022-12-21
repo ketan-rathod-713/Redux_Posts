bdf9b823db8a691bd9fc36bedbd28d061d1e8acf
ketan <ketanrtd1@gmail.com>
2022-12-21 12:32:39 +0530

    added time

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,19 +2,23 @@ import { selectAllPosts } from "./postsSlice";
 import {useSelector} from "react-redux"
 import React from "react";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
 
     const posts = useSelector(selectAllPosts);
+
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
     // const posts = useSelector(state => state.posts);
     // what if the state changes, that's why it is good to import the selector that's why we don't need to change the each components 
 
-    const renderedPosts = posts.map(post =>(
+    const renderedPosts = orderedPosts.map(post =>(
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
             <p className="postCredit">
                 <PostAuthor userId={post.userId}></PostAuthor>
+                <TimeAgo timestamp={post.date}></TimeAgo>
             </p>
         </article>
     ))
